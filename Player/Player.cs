10788910bdcbdb69921d2ce62e5a4831e2360c3c
Waffi <thewafflesdiscord@gmail.com>
2025-09-28 20:38:27 -0400

    FIXED BUGS

--- Player/Player.cs
+++ Player/Player.cs
@@ -19,23 +19,23 @@ public partial class Player : CharacterBody2D
 	{
 		base._PhysicsProcess(delta);
 
-        getInput();
+		getInput();
 
-        GD.Print(GlobalPosition);
-    }
+		GD.Print(GlobalPosition);
+	}
 
 	public void die() {
 		GD.Print("died");
 	}
 
-    public void isPlayer() {}
+	public void isPlayer() {}
 
-    public void getInput()
+	public void getInput()
 	{
 		Vector2 InputDir = Input.GetVector("a", "d", "w", "s");
 
-        GD.Print("Inputdir: ", InputDir);
+		GD.Print("Inputdir: ", InputDir);
 
-        Velocity = InputDir.Normalized() * Speed;
+		Velocity = InputDir.Normalized() * Speed;
 	}
 }
